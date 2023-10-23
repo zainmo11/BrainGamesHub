64dfff59a3654f54e7115b5129bc9b6f9c9c8cc4
Abdelrhman <zyn6681@gmail.com>
2023-10-23 23:25:27 +0300

    my version 0.0.3

--- myFirstApp/settings.py
+++ myFirstApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x6+8at=6jgs)b4y(u!43pd%+t-s7xt(8b10ogmv-k*3-jr95$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['braingameshub-production.up.railway.app']
+ALLOWED_HOSTS = ['braingameshub-production.up.railway.app','http://localhost:4200/']
 
 CSRF_TRUSTED_ORIGINS = ['https://braingameshub-production.up.railway.app']
 
