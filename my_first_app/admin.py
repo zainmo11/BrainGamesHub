17cb85d69ba59cc01cc5acf02a7d10fd0541fa14
Abdelrhman <zyn6681@gmail.com>
2023-10-26 00:40:09 +0300

    my version 0.0.4

--- my_first_app/admin.py
+++ my_first_app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import EasyLevel, MediumLevel, HardLevel
-
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 # Register your models here.
 
 admin.site.register(EasyLevel)
 admin.site.register(MediumLevel)
 admin.site.register(HardLevel)
+
+
+#
