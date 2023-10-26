a58b47340ed0e7c6f6cff7463c888ad395087340
Abdelrhman <zyn6681@gmail.com>
2023-10-26 18:21:46 +0300

    my version 0.0.4

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -5,14 +5,14 @@ class EasyLevel(models.Model):
     Participant_Name = models.CharField(max_length=50)
     Submission_Time = models.DateTimeField()
     Score = models.IntegerField()
-    level_params_digits = models.CharField(max_length=50)
+    level_params_digits = models.CharField(max_length=50,default="easy-1-1")
 class MediumLevel(models.Model):
-    Participant_Name = models.CharField(max_length=50)
+    Participant_Name = models.CharField(max_length=50,)
     Submission_Time = models.DateTimeField()
     Score = models.IntegerField()
-    level_params_digits = models.CharField(max_length=50)
+    level_params_digits = models.CharField(max_length=50,default="easy-1-1")
 class HardLevel(models.Model):
     Participant_Name = models.CharField(max_length=50)
     Submission_Time = models.DateTimeField()
     Score = models.IntegerField()
-    level_params_digits = models.CharField(max_length=50)
+    level_params_digits = models.CharField(max_length=50,default="easy-1-1")
